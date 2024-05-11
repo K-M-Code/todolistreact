2864fc290615941378dfdf772c648f938decb7e2
kmcode <kmcode@kmpc>
2024-05-11 21:38:56 +0300

    2nd Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState, useEffect } from 'react';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-material.css';
+
 
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -13,80 +12,73 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 
-import AddToDo from './AddToDo';
+import AddBooks from './AddBooks';
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [books, setBooks] = useState([]);
 
   const columnDefs = [
-    { field: 'description', sortable: true, filter: true, suppressMovable:true},
-    { field: 'date', sortable: true, filter: true, suppressMovable:true},
-    { field: 'priority', sortable: true, filter: true, suppressMovable:true},
+    { headerName: 'Title', field: 'title', sortable: true, filter: true, suppressMovable:true},
+    { headerName: 'Author', field: 'author', sortable: true, filter: true, suppressMovable:true},
+    { headerName: 'Year', field: 'year', sortable: true, filter: true, suppressMovable:true},
+    { headerName: 'ISBN', field: 'isbn', sortable: true, filter: true, suppressMovable:true},
+    { headerName: 'Price', field: 'price', sortable: true, filter: true, suppressMovable:true},
     { 
       headerName: '',
       field: 'id',
       width: 90,
-      cellRenderer: params => 
-      <IconButton onClick={() => deleteTodo(params.value)} size="small" color="error">
-        <DeleteIcon />
-      </IconButton> 
+      cellRenderer: params => (
+        <IconButton onClick={() => deleteBook(params.value)} size="small" color="error">
+          <DeleteIcon />
+        </IconButton>
+      )
     }
-  ]
+  ];
 
   useEffect(() => {
-    fetchItems();
-  }, [])
+    fetchBooks();
+  }, []);
 
-  const fetchItems = () => {
-    fetch('https://todolist-f614a-default-rtdb.europe-west1.firebasedatabase.app/items/.json')
-    .then(response => response.json())
-    .then(data => {
-      const todoArray = Object.keys(data).map(key => ({ ...data[key], id: key }));
-      setTodos(todoArray);
+  const fetchBooks = () => {
+    fetch('https://todolist-f614a-default-rtdb.europe-west1.firebasedatabase.app/books/.json')
+      .then(response => response.json())
+      .then(data => {
+        const bookArray = Object.keys(data).map(key => ({ ...data[key], id: key }));
+        setBooks(bookArray);
+      })
+      .catch(err => console.error(err));
+  };
+
+  const addBook = (newBook) => {
+    fetch('https://todolist-f614a-default-rtdb.europe-west1.firebasedatabase.app/books/.json', {
+      method: 'POST',
+      body: JSON.stringify(newBook)
     })
+    .then(response => fetchBooks())
     .catch(err => console.error(err));
   };
 
-  const addTodo = (newTodo) => {
-    fetch('https://todolist-f614a-default-rtdb.europe-west1.firebasedatabase.app/items/.json',
-    {
-      method: 'POST',
-      body: JSON.stringify(newTodo)
-    })
-    .then(response => fetchItems())
-    .catch(err => console.error(err))
-  }
-
-  const addKeys = (data) => {
-    const keys = Object.keys(data);
-    const valueKeys = Object.values(data).map((item, index) => 
-    Object.defineProperty(item, 'id', {value: keys[index]}));
-    setTodos(valueKeys);
-  }
-
-  const deleteTodo = (id) => {
-    fetch(`https://todolist-f614a-default-rtdb.europe-west1.firebasedatabase.app/items/${id}.json`,
-    {
+  const deleteBook = (id) => {
+    fetch(`https://todolist-f614a-default-rtdb.europe-west1.firebasedatabase.app/books/${id}.json`, {
       method: 'DELETE',
     })
-    .then(response => fetchItems())
-    .catch(err => console.error(err))
-  }
-
+    .then(response => fetchBooks())
+    .catch(err => console.error(err));
+  };
 
   return (
     <>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h5">
-            TodoList
+            Bookstore
           </Typography>
         </Toolbar>
-      </AppBar> 
-      <AddToDo addTodo={addTodo}/>
-      <div className="ag-theme-material" style={{height: 400, width: 690}}> 
-        <AgGridReact 
-          rowData={todos}
+      </AppBar>
+      <AddBooks addBook={addBook} />
+      <div className="ag-theme-material" style={{ height: 400, width: '90vw' }}>
+        <AgGridReact
+          rowData={books}
           columnDefs={columnDefs}
         />
       </div>
